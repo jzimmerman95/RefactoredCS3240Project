5fa58fd9c83a52b09a5d7a37f76743818e320bbc
clm5uz <clm5uz@virginia.edu>
2016-03-28 19:41:59 -0400

    Attempt 1 to fix views.py

--- myapplication/views.py
+++ myapplication/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def sign_up(request):
-        return render(request, 'myapplication/signUp.html', {})
+        return render(request, '/myapplication/signUp.html', {})
