619ded8ffa86b181dc83a060fcdd49872fa8cbf6
clm5uz <clm5uz@virginia.edu>
2016-03-28 19:58:19 -0400

    Attempt to update settings.py 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(__file__); # os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
