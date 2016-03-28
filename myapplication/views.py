4d992c41fe26e62c438e82ee8adb59e6ddd90bb4
clm5uz <clm5uz@virginia.edu>
2016-03-28 19:37:28 -0400

    Fixed a name error in views.py

--- myapplication/views.py
+++ myapplication/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def sign_up(request):
-        return render(request, 'myapplication/signIn.html', {})
+        return render(request, 'myapplication/signUp.html', {})
