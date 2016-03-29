897261e871697c8b663f51c8ccada1b8dc00311c
clm5uz <clm5uz@virginia.edu>
2016-03-28 20:41:44 -0400

    Another attempt to fix forms.py

--- myapplication/models.py
+++ myapplication/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class UserInformation(models.Model):
-	username = models.CharField(max_length=200)
-	password = models.CharField(max_length=200, default='none')
-	email = models.CharField(max_length=200, default='none')
-	firstname = models.CharField(max_length=200, default='none')
-	lastname = models.CharField(max_length=200, default='none')
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200, default='none')
+    email = models.CharField(max_length=200, default='none')
+    firstname = models.CharField(max_length=200, default='none')
+    lastname = models.CharField(max_length=200, default='none')
