f8c48b9b68eb97d02c9e3460e724fb9bfb875da7
clm5uz <clm5uz@virginia.edu>
2016-03-29 13:23:18 -0400

    Updated the home page url and updated the sign-up view in views.py, fixed the home button in failedLogin.html

--- myapplication/urls.py
+++ myapplication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^home_page$', views.home_page, name='home_page'),
     url(r'^sign_up$', views.sign_up, name='sign_up'),
     url(r'^sign_in$', views.sign_in, name='sign_in'),
     url(r'^sign_user_up$', views.sign_user_up, name='sign_user_up'),
