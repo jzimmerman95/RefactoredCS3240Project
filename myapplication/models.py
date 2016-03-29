44fae1b672b8e5d1a4cf366a55c095ae83992b97
clm5uz <clm5uz@virginia.edu>
2016-03-28 20:39:50 -0400

    Fixed indentations in forms.py

--- myapplication/models.py
+++ myapplication/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class UserInformation(models.Model):
 	username = models.CharField(max_length=200)
 	password = models.CharField(max_length=200, default='none')
