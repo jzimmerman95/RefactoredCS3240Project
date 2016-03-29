607d580d43f23315af65ee70db514356395decb9
clm5uz <clm5uz@virginia.edu>
2016-03-28 21:49:42 -0400

    Trying to see more errors

--- myapplication/models.py
+++ myapplication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class UserInformation(models.Model):
-	username = models.CharField(max_length=200)
+	username = models.CharField(max_length=200, default='none')
 	password = models.CharField(max_length=200, default='none')
 	email = models.CharField(max_length=200, default='none')
 	firstname = models.CharField(max_length=200, default='none')
