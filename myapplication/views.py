7ad1894ea04d8b27234f04d4bdfe030c5b1fe4fe
clm5uz <clm5uz@virginia.edu>
2016-03-28 22:23:54 -0400

    Included models in views.py

--- myapplication/views.py
+++ myapplication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import UserSignUpForm
+from .models import UserInformation
 
 # Create your views here.
 def sign_up(request):
