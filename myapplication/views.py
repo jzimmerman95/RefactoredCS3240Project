c8c00142c6906729e03f3d1e4ec44492479139e6
clm5uz <clm5uz@virginia.edu>
2016-03-28 22:34:02 -0400

    Trying a new way to save form data

--- myapplication/views.py
+++ myapplication/views.py
@@ -11,14 +11,14 @@ def sign_user_in(request):
 	if request.method == 'POST':
 		form = UserSignUpForm(request.POST)
 		if form.is_valid():
-			username = request.POST.get('username', '')
-			print(username);
-			pwd = request.POST.get('password', '')
-			email = request.POST.get('email', '')
-			fname = request.POST.get('fname', '')
-			lname = request.POST.get('lname', '')
-			user_inf_obj = UserInformation(username = username, password = pwd, email = email, firstname = "none", lastname = "none")
-			user_inf_obj.save()
+			#username = request.POST.get('username', '')
+			#pwd = request.POST.get('password', '')
+			#email = request.POST.get('email', '')
+			#fname = request.POST.get('fname', '')
+			#lname = request.POST.get('lname', '')
+			#user_inf_obj = UserInformation(username = username, password = pwd, email = email, firstname = fname, lastname = lname)
+			#user_inf_obj.save()
+			form.save()
 			return HttpResponseRedirect('')
 	else:
 		form = UserSignUpForm()
