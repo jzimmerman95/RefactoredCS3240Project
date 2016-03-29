9b2a39874f9621840a71e90a3a658e593994afe7
clm5uz <clm5uz@virginia.edu>
2016-03-29 09:52:22 -0400

    Added text to home page and sign-in page, updated the email field in the UserInformation model to be an EmailField

--- myapplication/models.py
+++ myapplication/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class UserInformation(models.Model):
 	username = models.CharField(max_length=200, default='none')
 	password = models.CharField(max_length=200, default='none')
-	email = models.CharField(max_length=200, default='none')
+	email = models.EmailField(max_length=200, default='none')
 	firstname = models.CharField(max_length=200, default='none')
 	lastname = models.CharField(max_length=200, default='none')
