fd2b1008c92674027f9f4fb2c077dc3af4cbd928
jzimmerman95 <jessicazimmerman95@gmail.com>
2016-04-20 11:58:14 -0400

    database migrations

---
+++ myapplication/migrations/0030_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('myapplication', '0029_userinformation_numsitemanagersmade'),
+        ('myapplication', '0003_groups'),
+    ]
+
+    operations = [
+    ]
