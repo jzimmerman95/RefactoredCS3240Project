63d5caacafb069bd4faaa57da8debce9735de0b9
clm5uz <clm5uz@virginia.edu>
2016-04-21 23:00:46 -0400

    User can view reports he/she owns and that are shared with him/her

--- FDA.py
+++ FDA.py
@@ -8,6 +8,8 @@ django.setup()
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout
 from myapplication.models import Report, ReportFiles, ReportGroups, Groups
+# imports for encrytion
+from Crypto.PublicKey import RSA
 
 def promptLogin():
 	user = input('Please enter username: ')
@@ -48,56 +50,114 @@ def menu(user):
 			tup = (str(count), r.reportname)
 			repList.append(tup)
 			count+=1
-		print("To view a report's files, type the report's corresponding number. To return to the main menu, type 'main'. To quit, type 'quit'")
+		print ("Options: ")
+		print("View a report's files by typing the report's corresponding number.")
+		print("Type 'main' to return to the main menu")
+		print("Type 'quit' to quit")
 		subMenuChoice = input()
+		fileList = []
 		while(subMenuChoice != 'quit'):
 			if subMenuChoice == 'main':
 				menu(user)
+			elif 'download' in subMenuChoice:
+				# downloading a file
+				download(fileList, subMenuChoice)
 			else:
-				found = show_files(subMenuChoice, repList)
+				found = show_files(subMenuChoice, repList, fileList)
 				if found == False:
-					print("The report you selected does not exist. View the files from another report by typing its corresponding number, type 'main' to see main menu options, or type 'quit' to quit.")
+					print("The report you selected does not exist.")
+					print ("Options: ")
+					print("View a report's files by typing the report's corresponding number.")
+					print("Type 'main' to return to the main menu")
+					print("Type 'quit' to quit")
 				else:
-					print("View the files from another report by typing its corresponding number, type 'main' to see main menu options, or type 'quit' to quit.")
+					print ("Options: ")
+					print("View a report's files by typing the report's corresponding number.")
+					print("Download a file by typing 'download' followed by the file's corresponding number")
+					print("Type 'main' to return to the main menu")
+					print("Type 'quit' to quit")
 			subMenuChoice=input()
+		if subMenuChoice == 'quit':
+			print("Goodbye.")
+			return
 	elif mainMenuChoice == '2':
 		# view shared reports
 		groups = Groups.objects.filter(username=user)
+		print("Report Name\tSummary\tDescription\tPublic/Private\tTime of Creation\tGroup")
 		for group in groups:
-			print(group.groupname+" reports: ")
-			print("Report Name\tSummary\tDescription\tPublic/Private\tTime of Creation")
 			count = 1
 			repList = []
 			for r in ReportGroups.objects.filter(groupname=group.groupname):
 				r = Report.objects.get(reportname=r.reportname)
-				print(str(count)+": "+r.reportname+"\t"+r.summary+"\t"+r.description+"\t"+r.isprivate+"\t"+str(r.timestamp))
+				print(str(count)+": "+r.reportname+"\t"+r.summary+"\t"+r.description+"\t"+r.isprivate+"\t"+str(r.timestamp)+"\t"+group.groupname)
 				tup = (str(count), r.reportname)
 				repList.append(tup)
 				count+=1
 		print("To view a report's files, type the report's corresponding number. To return to the main menu, type 'main'. To quit, type 'quit'")
 		subMenuChoice = input()
+		fileList = []
 		while (subMenuChoice != 'quit'):
 			if subMenuChoice == 'main':
 				menu(user)
+			elif 'download' in subMenuChoice:
+				# downloading a file
+				download(fileList, subMenuChoice)
 			else:
-				found = show_files(subMenuChoice, repList)
+				found = show_files(subMenuChoice, repList, fileList)
 				if found == False:
-					print("The report you selected does not exist. View the files from another report by typing its corresponding number, type 'main' to see main menu options, or type 'quit' to quit.")
+					print("The report you selected does not exist.")
+					print ("Options: ")
+					print("View a report's files by typing the report's corresponding number.")
+					print("Type 'main' to return to the main menu")
+					print("Type 'quit' to quit")
 				else:
-					print("View the files from another report by typing its corresponding number, type 'main' to see main menu options, or type 'quit' to quit.")
+					print ("Options: ")
+					print("View a report's files by typing the report's corresponding number.")
+					print("Download a file by typing 'download #', where # is the file's corresponding number")
+					print("Type 'main' to return to the main menu")
+					print("Type 'quit' to quit")				
 			subMenuChoice = input()
+		if subMenuChoice == 'quit':
+			print("Goodbye.")
+			return
 	print("Goodbye.")
 	return
 
-def show_files(subMenuChoice, repList):
+def show_files(subMenuChoice, repList, fileList):
 	found = False
+	del fileList[:]
 	for tup in repList:
 		if tup[0] == subMenuChoice:
 			# display the files from this report
+			count = 1
 			for f in ReportFiles.objects.filter(reportname=tup[1]):
-				print(f.uploadfile.name)
+				tup = (str(count), f)
+				fileList.append(tup)
+				if f.isencrypted == 1:
+					print(str(count)+": "+f.uploadfile.name+" \t(encrypted)")
+				else:
+					print(str(count)+": "+f.uploadfile.name)
+				count+=1
 			return True
 	return False
+
+def download(fileList, subMenuChoice):
+	num = subMenuChoice[9:]
+	for f in fileList:
+		if f[0] == num:
+			if f[1].isencrypted == 1:
+				download_encrypted_file(f[1].uploadfile.name)
+			else:
+				download_unencrypted_file(f[1].uploadfile.name)
+
+def download_unencrypted_file(filename):
+	print("Download "+filename)
+	
+def download_encrypted_file(filename):
+	print("Please enter the decryption key: ")
+	key = input()
+	#key.decrypt("something")
+	print("Download "+filename)
 
 if __name__=="__main__":
 	user = input('Please enter username: ')
